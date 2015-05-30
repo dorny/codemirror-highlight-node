886cad117d9c357b887e507b76536c70a73d6b4d
Michal Dorner <dorner.michal@gmail.com>
2015-05-30 20:04:52 +0200

    use codemirror source-highlight executable for tests expected output

--- test/index.js
+++ test/index.js
@@ -6,17 +6,14 @@ var path = require('path');
 
 var it = require('testit');
 var highlight = require('../');
+var child_process = require('child_process');
 
 var srcPath = path.join(__dirname,'fixtures','src');
-var outputPath = path.join(__dirname,'fixtures','output');
 var srcFiles = fs.readdirSync(srcPath);
+var source_highlight = path.join(__dirname, '..', 'node_modules', 'codemirror', 'bin', 'source-highlight');
 
 function readSrcFile(name) {
   return fs.readFileSync(path.join(srcPath, name), {encoding: 'utf8'});
-}
-
-function readOutputFile(name) {
-  return fs.readFileSync(path.join(outputPath, name+'.html'), {encoding: 'utf8'});
 }
 
 srcFiles.forEach( function(file) {
@@ -24,8 +21,16 @@ srcFiles.forEach( function(file) {
   it('Generate formated HTML from '+mode+' code', function() {
     var source = readSrcFile(file);
     var result = highlight(source, mode);
-    var expected = readOutputFile(mode);
-    assert.equal(result, expected);
+    var mime = highlight.CodeMirror.findModeByName(mode).mime;
+
+    var child = child_process.exec( source_highlight, ['-s', mime], {encoding: 'utf8'},
+      function(error, stdout, stderr) {
+        assert(error == null);
+        assert.equal(result, stdout);
+    });
+
+    child.stdin.write(source);
+    child.stdin.end();
   });
 });
 
